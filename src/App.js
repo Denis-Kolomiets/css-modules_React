4e534115ff4a302e8e7ce07eaed88adc6edf7bd6
Denis kolomiets <deniskolomiets988@mail.com>
2022-12-14 12:42:19 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css'
+import Info from './components/Info'
+
+function App() {
+  return (
+    <div className="App">
+      <Info />
+
+      <div className="info">
+        <h1>H1 component App</h1>
+        <h2>Its h2 App.js file</h2>
+        <button className="my-button-info">App button</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
